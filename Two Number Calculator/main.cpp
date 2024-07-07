69680082810fef8f5f9660e5973461c700f591a7
Alexander Nesterov <alex18nest@icloud.com>
2024-07-07 17:09:26 -0300

    Initial Commit

---
+++ Two Number Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Two Number Calculator
+//
+//  Created by Александр Нестеров on 07/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
